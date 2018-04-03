178e9908a9585709c610ac023620872e2c2d0ff8
Your Name <you@example.com>
2018-04-03 13:00:32 +0800

    a beta version

--- setup.py
+++ setup.py
@@ -3,18 +3,18 @@
 from setuptools import setup, find_packages
 
 
-setup(name='P',
-    version='0.1',
+setup(name='mroy-line',
+    version='0.2',
     description='a simple way to use mongo db, let db like dict',
     url='https://github.com/Qingluan/.git',
-    author='Qing luan',
+    author='Froy_Qing',
     author_email='darkhackdevil@gmail.com',
     license='MIT',
     zip_safe=False,
     packages=find_packages(),
     install_requires=['termcolor','dill'],
     entry_points={
-        'console_scripts': ['ppx=P.cmd:run']
+        'console_scripts': ['rp=P.cmd:run']
     },
 )
 
